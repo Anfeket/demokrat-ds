b59594400a2a52862c653ec39363f17fb6ab2a70
Anfeket <anfeket@gmail.com>
2022-04-11 22:35:11 +0200

    fix

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ client.on("messageCreate", (message) => {
     } catch (error) {
       console.error(error);
       message.reply("there was an error trying to execute that command!");
-    };
+    }
 });
 
 // login
